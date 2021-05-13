a09b84eee227f3e3cf790f108931730ebcb0e748
jqsook@yahoo.com <you@example.com>
2021-05-13 13:07:34 -0600

    updt

--- game.js
+++ game.js
@@ -2,13 +2,15 @@ var health = 100
 
 function slap() {
     health = (health- 1)
-    // alert(health)
+    console.log(health) //This works in console.
     return health
     // update() 
 }
 function update() {
+    let template = ''
+    template +=
     const healthElem = document.getElementById("health")
     healthElem?.innerHTML = `
         <span>Health:<span id="health">${healthElem}</span></span>
     `
-}update()
+}
